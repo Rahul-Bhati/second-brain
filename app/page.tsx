1dbe7203012a3e41125a0e13614cce99fbe924e7
Rahul-Bhati <rb083380@gmail.com>
2024-11-22 21:20:05 +0530

    d

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,11 @@ export default function Home() {
     <>
       <Header />
       <h1>Hello</h1>
+      <div className="flex justify-center">
+        <h1 className="flex justify-center w-[70%] bg-blue-50 border border-blue-300 text-blue-800 text-sm rounded-lg p-4">
+          <strong className="font-semibold">Note:  </strong> Landing page and some functionality is under development, but for now you can store tweets and query/search for them using AI/LLMs.
+        </h1>
+      </div>
 
     </>
   );
